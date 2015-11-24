89e6ffb0db76c6c8b07e135b38c3ce17a417ed02
vvkdby <vvkdubey@ucdavis.edu>
2015-11-23 20:17:08 -0800

    Mining twitter for tweets with relevant keywords

---
+++ twitter_streaming.py
@@ -0,0 +1,42 @@
+#Import the necessary methods from tweepy library
+import timeit
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+
+
+#Variables that contains the user credentials to access Twitter API
+access_token = "1393149704-myE7HPjI0x0jRmGXcEw2aag5fsyblNRWQaZU7wZ"
+access_token_secret = "MDE2tPpSSWoVkdwUNdgymwbhtCYHrYk88AGxpSTTYm7CT"
+consumer_key = "8bohnVjqvmiEXRXw4pph2T4go"
+consumer_secret = "H7Qc6krY9rbIZpUg03AXb93huNsJe7xd4a2PzutYTpz3uzSLSJ"
+
+start = timeit.default_timer()
+#This is a basic listener that just prints received tweets to stdout.
+class StdOutListener(StreamListener):
+
+    def on_data(self, data):
+        print data
+        return True
+
+    def on_error(self, status):
+        print status
+
+
+if __name__ == '__main__':
+
+    #This handles Twitter authetification and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    stream = Stream(auth, l)
+
+    #This line filter Twitter Streams to capture data by the following keywords
+    #stream.filter(track=['classroom' ,'education','school','college','university'])
+    stream.filter(track=['business', 'marketing', 'advertisement', 'buy','sell','co-founder','entrepreneurship'])
+    #stream.filter(track=['entertainment'])
+    #stream.filter(track=['technology'])
+    #stream.filter(track=['environment'])
+
+stop = timeit.default_timer()
+print stop-start 
