407411bcef7920f49d0108e2b0f3b035e5c6ff05
Yagnik Suchak <yvsuchak@ucdavis.edu>
2015-12-02 21:22:36 -0800

    created a function

--- preProcessing.py
+++ preProcessing.py
@@ -62,50 +62,61 @@ entertainment=['entertainment','movies','sports','TV','Hollywood','Opera']
 technology=['technology','science','gadgets','machines','software','hardware']
 environment=['environment','nature','earth','plants','animals','planet','greenhouse','air']
 
-X_TrainMain=[]
-Y_TrainMain=[]
 
+def createFeatureVector(filename,X_TrainMain,Y_TrainMain):
 
+    with open(filename, 'r') as f:
+        for line in f:
+            if not line.strip():
+                continue
+            # tweet = json.loads(line)
+            # print line
+            tweet = json.loads(line)
+            # tokens = preprocess(tweet['text'])
+            #Feature vector per tweet.
+            X_Train=[0]*32
+            #Target variable per tweet.
+            Y_Train=1
 
-with open('sample_data.txt', 'r') as f:
-    for line in f:
-        if not line.strip():
-            continue
-        # tweet = json.loads(line)
-        # print line
-        tweet = json.loads(line)
-        # tokens = preprocess(tweet['text'])
-        #Feature vector per tweet.
-        X_Train=[0]*32
-        #Target variable per tweet.
-        Y_Train=1
+            if 'text' not in tweet:
+                continue
 
-        if 'text' not in tweet:
-            continue
+            tokens=[lmtzr.lemmatize(term) for term in preprocess(tweet['text']) if term not in stop]
+            # print tokens
 
-        tokens=[lmtzr.lemmatize(term) for term in preprocess(tweet['text']) if term not in stop]
-        # print tokens
+            for subClass in tokens:
+                subClass=subClass.lower()
+                if subClass in business:
+                    X_Train[business.index(subClass)]=1
+                if subClass in education:
+                    X_Train[education.index(subClass)+7]=1
+                if subClass in entertainment:
+                    X_Train[entertainment.index(subClass)+12]=1
+                if subClass in technology:
+                    X_Train[technology.index(subClass)+18]=1
+                if subClass in environment:
+                    X_Train[environment.index(subClass)+24]=1
 
-        for subClass in tokens:
-            subClass=subClass.lower()
-            if subClass in business:
-                X_Train[business.index(subClass)]=1
-            if subClass in education:
-                X_Train[education.index(subClass)+7]=1
-            if subClass in entertainment:
-                X_Train[entertainment.index(subClass)+12]=1
-            if subClass in technology:
-                X_Train[technology.index(subClass)+18]=1
-            if subClass in environment:
-                X_Train[environment.index(subClass)+24]=1
+            X_TrainMain.append(X_Train)
+            Y_TrainMain.append(Y_Train)
 
-        X_TrainMain.append(X_Train)
-        Y_TrainMain.append(Y_Train)
+    return X_TrainMain,Y_TrainMain
+
 
         # print X_Train
 
-print Y_TrainMain
+# print Y_TrainMain
 # print X_TrainMain
-df = pandas.DataFrame(X_TrainMain)
-df['y']=Y_TrainMain
-print df
+# df = pandas.DataFrame(X_TrainMain)
+# df['y']=Y_TrainMain
+# print df
+
+X_TrainMain=[]
+Y_TrainMain=[]
+
+X_TrainMain,Y_TrainMain=createFeatureVector("twitter_data_business.txt",X_TrainMain,Y_TrainMain)
+X_TrainMain,Y_TrainMain=createFeatureVector("twitter_data_education.txt",X_TrainMain,Y_TrainMain)
+X_TrainMain,Y_TrainMain=createFeatureVector("twitter_data_entertainment.txt",X_TrainMain,Y_TrainMain)
+X_TrainMain,Y_TrainMain=createFeatureVector("twitter_data_technology.txt",X_TrainMain,Y_TrainMain)
+X_TrainMain,Y_TrainMain=createFeatureVector("twitter_data_environment.txt",X_TrainMain,Y_TrainMain)
+
